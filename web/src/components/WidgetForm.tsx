bafbece9b946eacfc9a750b630f565aa637adab6
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-05-04 16:23:53 -0400

    Estilizando buttons de feedback e destacando com hover

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -52,7 +52,10 @@ export function WidgetForm() {
 
                 { Object.entries(feedbackTypes).map(([key, value]) => {
                     return (
-                        <button key={key}>
+                        <button key={key}
+                         className="bg-zinc-800 rounded-lg py-5 w-24 flex flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none "
+                         type="button"
+                        >
                             <img src={value.image.source} alt={value.image.alt} />
                             <span>{value.title}</span>
                         </button>
