e4e6598f968508be128d794b7e2699134116527b
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-05-04 18:37:02 -0400

    FeedBackContentStep - botão voltar

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { CloseButton } from "../closeButton";
 
 import bugImageUrl from '../../assets/bug.svg'
 import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 /** Vetor de objetos, acessado pela propriedade:
  * 
@@ -48,14 +48,22 @@ export function WidgetForm() {
 
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+    function handleRestartFeedback() {
+        setFeedbackType(null)
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg
         w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-                <CloseButton />
-            </header>
+
+            {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+                <FeedbackContentStep feedbackType={feedbackType}
+                onFeedbackRestartRequested={handleRestartFeedback}
+                />
+            )}
+
             <footer>
                 Feito com ♥ pela <a className="underline underline-offset-2" href="#">Rocketseat</a>
             </footer>
