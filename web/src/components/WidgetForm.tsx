ef872a9e800672de9360e6d63a7828e4e390d7aa
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-05-04 17:10:21 -0400

    Criando um estado para o botão de feedback

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import { CloseButton } from "./closeBurron";
 
 import bugImageUrl from '../assets/bug.svg'
 import ideaImageUrl from '../assets/idea.svg'
 import thoughtImageUrl from '../assets/thought.svg'
+
+/** Vetor de objetos, acessado pela propriedade:
+ * 
+ *      //Object.entries(feedbackTypes) => 
+ * 
+ * [
+ * ['BUG', {...}],
+ * ['IDEA', {...}],
+ * ['THOUGHT', {...}]
+ * ]
+ */
 
 const feedbackTypes = {
     BUG: {
@@ -29,18 +41,12 @@ const feedbackTypes = {
 
 }
 
-/** Vetor de objetos, acessado pela propriedade:
- * 
- *      //Object.entries(feedbackTypes) => 
- * 
- * [
- * ['BUG', {...}],
- * ['IDEA', {...}],
- * ['THOUGHT', {...}]
- * ]
- */
+type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
+
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg
         w-[calc(100vw-2rem)] md:w-auto">
@@ -54,6 +60,7 @@ export function WidgetForm() {
                     return (
                         <button key={key}
                          className="bg-zinc-800 rounded-lg py-5 w-24 flex flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none "
+                         onClick = {() => setFeedbackType(key as FeedbackType)}
                          type="button"
                         >
                             <img src={value.image.source} alt={value.image.alt} />
