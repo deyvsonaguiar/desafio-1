eb255d8c32463da3d14e0061a1f94bf1841b05ee
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-05-04 22:57:50 -0400

    FeedbackContentStep - botão EnviarFeedback

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -44,6 +44,18 @@ export function FeedbackContentStep({
                 scrollbar-thin"
                 placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
                 ></textarea>
+
+                <footer className="flex gap-2 mt-2">
+                    <button
+                    type="submit"
+                    className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex
+                    justify-center items-center text-sm hover:bg-brand-300 focus:outline-none
+                    focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 
+                    focus:ring-brand-500 transition-colors"
+                    >
+                    Enviar feedback
+                    </button>
+                </footer>
             </form>
         </>
     )
